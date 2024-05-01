bd1b8b3b73f34afcffa1cd9e217f5dcecacd6904
Christian-Tesfaye <128823771+Christian-Tesfaye@users.noreply.github.com>
2024-05-01 13:50:33 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -5,5 +5,9 @@ def Add(x,y):
     sum = x+y
     return sum
 
+def Subtract(x, y):
+  difference = x - y
+  return difference
+    
 print("sum is: ", Add(a,b))
-
+print("difference is: ", Subtract(b, a))
